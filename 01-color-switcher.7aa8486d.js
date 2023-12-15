b15fdcd4443f96a083077a53d52cc3fb54d74bd7
petruvlad <125945650+petruvlad@users.noreply.github.com>
2023-12-15 23:04:27 +0000

    Deploying to gh-pages from @ petruvlad/goit-js-hw-09@15c620897742c9f01e5ffb12212a805b78c7ce9a 🚀

---
+++ 01-color-switcher.7aa8486d.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var t;document.getElementById("startButton").addEventListener("click",(function(){this.disabled=!0,document.getElementById("stopButton").disabled=!1,t=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3)})),document.getElementById("stopButton").addEventListener("click",(function(){document.getElementById("startButton").disabled=!1,this.disabled=!0,clearInterval(t)}))}));
+//# sourceMappingURL=01-color-switcher.7aa8486d.js.map
