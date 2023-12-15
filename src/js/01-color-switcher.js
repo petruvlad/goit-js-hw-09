bcaebe5d01df93f13b8bc708733a1a3266761fef
petruvlad <vlad.petrisor31@gmail.com>
2023-12-16 01:08:57 +0200

    6th commit
    modificare script la exercitiul 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -8,22 +8,21 @@ document.addEventListener('DOMContentLoaded', function () {
   let intervalId;
 
   document.getElementById('startButton').addEventListener('click', function () {
-    // Dezactivează butonul "Start" și activează butonul "Stop"
+   
     this.disabled = true;
     document.getElementById('stopButton').disabled = false;
 
-    // Schimbă culoarea de fundal la fiecare secundă
+    
     intervalId = setInterval(function () {
       document.body.style.backgroundColor = getRandomHexColor();
     }, 1000);
   });
 
   document.getElementById('stopButton').addEventListener('click', function () {
-    // Activează butonul "Start" și dezactivează butonul "Stop"
+    
     document.getElementById('startButton').disabled = false;
     this.disabled = true;
 
-    // Oprește schimbarea culorii de fundal
     clearInterval(intervalId);
   });
 });
